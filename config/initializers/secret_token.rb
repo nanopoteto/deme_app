be8c33c32f01ee39464b5568bebaef49514dd2f0
夏目亮太 <nano.poteto@gmail.com>
2015-10-14 11:14:28 +0900

    Initail commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemeApp::Application.config.secret_key_base = '8e92c72ebbf6e398a31c44161f4b104a55d197060e2c69b1ae1ed91b710616d82cd78b93395b933a58b563cea145fcd1c8622177ba3b07f2a3ab743922ba6c20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DemeApp::Application.config.session_store :cookie_store, key: '_deme_app_session'
